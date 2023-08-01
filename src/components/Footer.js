e37679808f6e131988c3de160fd917237be6436e
Rutuja Kale <129681496+RutujaDevidasKale@users.noreply.github.com>
2023-08-01 23:39:39 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const company = "FlipIt";
+  return (
+    <footer>
+      <p>Copyright ⓒ {company}</p>
+    </footer>
+  );
+}
+
+export default Footer;
